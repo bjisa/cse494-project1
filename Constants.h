6630f554ceb683e38a3ed2d1e603776fa823e8d7
bjisa <benjisa@gmail.com>
2014-10-22 13:29:08 -0700

    merge

---
+++ Constants.h
@@ -0,0 +1,15 @@
+//
+//  Constants.h
+//  FlyWhizz
+//
+//  Created by Joshua Baldwin on 10/22/14.
+//  Copyright (c) 2014 ASU CSE 494. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Constants : NSObject
+
+extern NSString *const apiKey;
+
+@end
---
+++ Constants.m
@@ -0,0 +1,15 @@
+//
+//  Constants.m
+//  FlyWhizz
+//
+//  Created by Joshua Baldwin on 10/22/14.
+//  Copyright (c) 2014 ASU CSE 494. All rights reserved.
+//
+
+#import "Constants.h"
+
+@implementation Constants
+
+NSString *const apiKey = @"SOME_API_KEY_VALUE_MAKE_IT_REAL_LONG_AND_EVEN_LONGER_AND_FILL_IT_WITH_NOTHING_SUPER_IMPORTANT_AT_ALL_0123456789ABCDEF";
+
+@end
